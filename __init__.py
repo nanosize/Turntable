0f0e7ef783fbbc77a516b802dbc0ede3fc61ae58
nanosize <145854377+nanosize@users.noreply.github.com>
2023-10-03 14:04:54 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
    "name": "turntable",
    "author": "nao_3dcg",
-   "version": (0, 0, 0),
+   "version": (1, 0, 0),
    "blender": (3, 0, 0),
    "location": "View3D > turntable",
    "description": "It automatically creates and animates the turntable.",
@@ -13,120 +13,90 @@ bl_info = {
 
 import bpy
 
-#UI set
+# UI set
 class VIEW3D_PT_track_obj(bpy.types.Panel):
 
-     #where to add the panel
+    # where to add the panel
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Turntable"
     bl_label = "Turntable"
-    
+
     def draw(self, context):
         layout = self.layout
         scene = context.scene
-        
+
         # Frame
         layout.prop(scene, "turntable_frames", text="Frames")
-        
-        #layout.separator()
-        
-        #create button
-        #layout.label(text="Create Camera:",icon="CAMERA_DATA")
+
+        # create button
         row = layout.row()
         row.scale_y = 3.0
-        row.operator("object.create", text="Create Camera",icon="CAMERA_DATA")
-        
+        row.operator("object.create", text="Create Camera", icon="CAMERA_DATA")
+
         layout.separator()
-        
-        #layout.operator("render.render", text="Start Animation Render",icon = "RENDER_ANIMATION")
-        
-        #delete
+
+        # delete
         layout.operator("object.delete_objects", text="Delete Objects", icon="CANCEL")
 
 
-
-    
-class  OBJECT_TO_create(bpy.types.Operator):
+class OBJECT_TO_create(bpy.types.Operator):
     bl_idname = "object.create"
     bl_label = "Tool Name"
-    
+
     def execute(self, context):
-        
+
         frm = bpy.context.scene.turntable_frames
-        
+
         global cam
         cam = None
-        
+
         if not bpy.data.collections.get('turntable'):
-            
-            bpy.ops.outliner.item_activate(deselect_all=True)
- 
+            # Create a new collection if it doesn't exist
             tr_obj = bpy.data.collections.new('turntable')
-            bpy.data.collections['turntable'].color_tag = 'COLOR_04'
             bpy.context.scene.collection.children.link(tr_obj)
-            
-            #add circle
-            bpy.ops.curve.primitive_bezier_circle_add(radius=5,location=(0,
-            0, 1), scale=(1, 1, 1))
+
+            # Add circle
+            bpy.ops.curve.primitive_bezier_circle_add(radius=5, location=(0, 0, 1), scale=(1, 1, 1))
             circle = bpy.context.view_layer.objects.active
             circle.name = 'turntable_circle'
 
-            #add empty
-            bpy.ops.object.empty_add() 
+            # Add empty
+            bpy.ops.object.empty_add()
             empty = bpy.context.view_layer.objects.active
             empty.name = 'turntable_focus_target'
 
-            #add camera
-            #get data about add objects
-            #change name
+            # Add camera
             bpy.ops.object.camera_add()
             cam = bpy.context.view_layer.objects.active
             bpy.context.scene.camera = cam
             cam.name = 'turntable_cam'
-            
-            #add object constraint follow_path
-            #set target
-            #animate
+
+            # Add object constraint follow_path
             bpy.ops.object.constraint_add(type='FOLLOW_PATH')
             cam.constraints["Follow Path"].target = circle
-            
-            #set start keyflame
+
+            # Set start keyframe
             cam.constraints["Follow Path"].offset = 0
-            cam.constraints["Follow Path"].keyframe_insert("offset",frame=1)
-            #set last keyflame
+            cam.constraints["Follow Path"].keyframe_insert("offset", frame=1)
+            # Set last keyframe
             cam.constraints["Follow Path"].offset = 100
-            cam.constraints["Follow Path"].keyframe_insert("offset",frame= frm)
-            #set framerange
-            
-            end_frame = bpy.context.scene.frame_end
+            cam.constraints["Follow Path"].keyframe_insert("offset", frame=frm)
+
+            # Set frame range
             end_frame = frm
-      
-            #add object constraint track to
+            bpy.context.scene.frame_end = frm
+
+            # Add object constraint track to
             bpy.ops.object.constraint_add(type='TRACK_TO')
             cam.constraints["Track To"].target = bpy.data.objects['turntable_focus_target']
-            
-            # set last frame
-            bpy.context.scene.frame_end = frm
-            
-            #link with track_collection
-            #should wright exception handling
+
+            # Link with turntable collection
             tr_obj.objects.link(circle)
             tr_obj.objects.link(cam)
             tr_obj.objects.link(empty)
-            bpy.context.scene.collection.objects.unlink(circle)
-            bpy.context.scene.collection.objects.unlink(cam)
-            bpy.context.scene.collection.objects.unlink(empty)
-            
-            return {'FINISHED'} 
-    '''
-        else:
-            if cam is not None:
-                cam.constraints["Follow Path"].keyframe_insert("offset", frame=frm)
-                bpy.context.scene.frame_end = frm
-                bpy.context.scene.turntable_frames = frm 
-    '''
-    
+
+        return {'FINISHED'}
 
 
 class OBJECT_delete_objects(bpy.types.Operator):
@@ -144,7 +114,7 @@ class OBJECT_delete_objects(bpy.types.Operator):
             bpy.data.collections.remove(bpy.data.collections['turntable'])
 
         return {'FINISHED'}
-    
+
 
 def register():
     bpy.utils.register_class(VIEW3D_PT_track_obj)
@@ -152,17 +122,17 @@ def register():
     bpy.utils.register_class(OBJECT_delete_objects)
     bpy.types.Scene.turntable_frames = bpy.props.IntProperty(
         name="Turntable Frames",
-        default=100,  # set defalt value
-        min=1,  # min
+        default=100,
+        min=1,
         description="Number of frames for the turntable animation"
     )
-    #bpy.utils.register_class()
-    #bpy.utils.register_class()
+
+
 def unregister():
-    bpy.utils.unregister_class(VIEW3D_PT_track_obj) 
+    bpy.utils.unregister_class(VIEW3D_PT_track_obj)
     bpy.utils.unregister_class(OBJECT_TO_create)
-    bpy.utils.register_class(OBJECT_delete_objects)
-    #bpy.utils.unregister_class()
-    #bpy.utils.unregister_class()
+    bpy.utils.unregister_class(OBJECT_delete_objects)
+
+
 if __name__ == "__main__":
     register()
